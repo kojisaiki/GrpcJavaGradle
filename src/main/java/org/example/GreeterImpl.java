d8f7d34b83e12910bc79dc66dc2225bf9d9c3232
Koji Saiki <saikou9901@gmail.com>
2021-05-05 18:38:16 +0900

    It worked!

--- src/main/java/org/example/GreeterImpl.java
+++ src/main/java/org/example/GreeterImpl.java
@@ -9,7 +9,7 @@ public class GreeterImpl extends GreeterGrpc.GreeterImplBase {
 
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
         responseObserver.onNext(reply);
         responseObserver.onCompleted();
     }
--- src/test/java/org/example/GreeterImplTest.java
+++ src/test/java/org/example/GreeterImplTest.java
@@ -1,17 +1,26 @@
 package org.example;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
-import org.junit.jupiter.api.BeforeAll;
+import io.grpc.ServerBuilder;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GreeterImplTest {
 
     private Server server;
 
-    @BeforeAll
-    public void beforeAll() {
+    @BeforeEach
+    public void beforeAll() throws IOException {
         server = ServerBuilder.forPort(6565)
                 .addService(new GreeterImpl())
                 .build();
@@ -19,8 +28,25 @@ class GreeterImplTest {
         server.start();
     }
 
+    @AfterEach
+    public void afterAll() {
+        server.shutdown();
+    }
+
     @Test
     public void clientTest() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
 
+        GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(channel);
+
+        HelloRequest request = HelloRequest.newBuilder()
+                .setName("Tom")
+                .build();
+
+        HelloReply reply = stub.sayHello(request);
+
+        assertEquals("Hello Tom", reply.getMessage());
     }
 }
